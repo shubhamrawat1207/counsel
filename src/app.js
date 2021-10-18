45a9caf9ddffd4b516d40efe5f92d29c1c8d9d3a
Shubham Rawat <rawatshubam269@gmail.com>
2021-10-18 17:56:50 +0530

    adding script

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000
+const app = express();
+
+app.set("view engine","hbs");
+const static_path = path.join(__dirname,"../public");
+
+app.use(express.static(static_path));
+app.get("/",(req,res)=>res.render("index"));
+
+app.listen(port,()=>console.log(`Server is running on port ${port}`));
+
